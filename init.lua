8c53428d4e8f6e8e8404510de52c9d07cdfbfc70
Ohukainen <johannes.kallstad@gmail.com>
2024-09-30 08:45:42 +0200

    add minor changes

--- init.lua
+++ init.lua
@@ -780,9 +780,9 @@ require('lazy').setup({
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'tomasr/molokai',
+    --> If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'embark-theme/vim',
+    'tomasr/molokai',
     'rebelot/kanagawa.nvim',
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
